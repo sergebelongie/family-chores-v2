12c60182f415407f14d03afe3109e4803bbc011e
Serge Belongie <sergebelongie@users.noreply.github.com>
2026-07-24 19:53:16 +0200

    copied over firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDBR0j1ZUCyhNgXX6ZFUWE0lFFN4vwyAgw",
+    authDomain: "family-chores-fm465.firebaseapp.com",
+    projectId: "family-chores-fm465",
+    storageBucket: "family-chores-fm465.appspot.com",
+    messagingSenderId: "554100140828",
+    appId: "1:554100140828:web:14153721b9a27035c228d9"
+  };
